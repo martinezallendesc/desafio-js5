609cfdc9175ddb10aaab5345b59c43a6e23e4fc9
Claudiazepam <martinezallendesclaudia@gmail.com>
2025-06-03 15:40:20 -0400

    correccioens

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,62 +9,60 @@ const listaTareas = document.getElementById("tabla-tareas")
 //2. Crear el arreglo
 const tareas = [
     {
-id: 1,
-tarea: "Desayunar",
-completada: true,
-}
-,
+        id: 1,
+        tarea: "Desayunar",
+        completada: true,
+    }
+    ,
 
-{
-    id: 2,
-    tarea: "Almorzar",
-    completada: false,
-}
+    {
+        id: 2,
+        tarea: "Almorzar",
+        completada: false,
+    }
 
-,
+    ,
 
-{
-    id: 3,
-    tarea: "Tomar 11",
-    completada: false,
-}
+    {
+        id: 3,
+        tarea: "Tomar 11",
+        completada: false,
+    }
 
-,
+    ,
 
-{
-    id: 4,
-    tarea: "Cenar",
-    completada: true,
-}
-  
+    {
+        id: 4,
+        tarea: "Cenar",
+        completada: true,
+    }
+
 
 ]
 
 // 3. Iniciar la acción: Presionar botón AGREGAR
 
 btnTarea.addEventListener("click", () => {
-const valor = inputTarea.value
+    const valor = inputTarea.value
 
-} )
+})
 
 // 4. Mostrar tareas en la tabla
 
-function mostrarTareas () {
-let texto =   ""  
-tareas.map( (tarea) => `
-<tr>
-<td>${tarea.id}</td>
-<td>${tarea.tarea}</td>
-<td><input type='checkbox' ${tarea.completada? 'checked="checked"':''}
-></td>
-<td><img src="./assets/img/cruz.png" style="width:10px"></img></td>
-</tr>
-` )
+function mostrarTareas() {
+    let texto = "";
 
-.forEach ((linea) => { texto += linea})
-listaTareas.innerHTML = texto
+    tareas.map((tarea) => `
+        <tr>
+            <td>${tarea.id}</td>
+            <td>${tarea.tarea}</td>
+            <td><input type='checkbox' ${tarea.completada ? 'checked="checked"' : ''}></td>
+            <td><img src="./assets/img/cruz.png" style="width:10px"></img></td>
+        </tr>
+        `)
+        .forEach((linea) => { texto += linea });
+
+    listaTareas.innerHTML = texto;
 }
 
-mostrarTareas()
-
-
+mostrarTareas();
